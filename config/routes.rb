468afc4653f9f60831839b7a0a2f6e51492bb712
Ken <strongcopy@gmail.com>
2012-06-15 20:58:57 -0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+  get "static_pages/about"
+
+end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +62,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
